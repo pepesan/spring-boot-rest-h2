d4bdc4959d5ba483c468a118b29541ce7dc207cc
pepesan <pepesan@gmailcom>
2019-02-11 04:17:11 +0100

    Initial commit

---
+++ src/main/java/com/cursosdedesarrollo/springbootresth2/SpringBootRestH2Application.java
@@ -0,0 +1,14 @@
+package com.cursosdedesarrollo.springbootresth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestH2Application.class, args);
+    }
+
+}
+
