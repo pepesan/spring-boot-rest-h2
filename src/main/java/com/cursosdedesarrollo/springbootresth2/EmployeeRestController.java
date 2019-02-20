ff1f0fd98e23a570eb9cad4587b6e9af3fd952c9
pepesan <pepesan@gmailcom>
2019-02-20 16:13:31 +0100

    mejorando los ejemplos

--- src/main/java/com/cursosdedesarrollo/springbootresth2/EmployeeRestController.java
+++ src/main/java/com/cursosdedesarrollo/springbootresth2/EmployeeRestController.java
@@ -2,34 +2,24 @@ package com.cursosdedesarrollo.springbootresth2;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@RequestMapping("/api/employees")
 public class EmployeeRestController {
 
     @Autowired
     private EmployeeService employeeService;
 
 
-    @GetMapping("/api/employees")
+    @GetMapping
     public List<Employee> getEmployees() {
         List<Employee> employees = employeeService.retrieveEmployees();
         return employees;
     }
 
-    @GetMapping("/api/employees/{employeeId}")
-    public Employee getEmployee(@PathVariable(name="employeeId")Long employeeId) {
-        return employeeService.getEmployee(employeeId);
-    }
-
-    @PostMapping("/api/employees")
+    @PostMapping
     public Employee saveEmployee(Employee employee){
         employeeService.saveEmployee(employee);
         System.out.println("Employee Saved Successfully");
@@ -37,7 +27,14 @@ public class EmployeeRestController {
         return employee;
     }
 
-    @DeleteMapping("/api/employees/{employeeId}")
+    @GetMapping("/{employeeId}")
+    public Employee getEmployee(@PathVariable(name="employeeId")Long employeeId) {
+        return employeeService.getEmployee(employeeId);
+    }
+
+
+
+    @DeleteMapping("/{employeeId}")
     public Employee deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
         Employee employee= employeeService.getEmployee(employeeId);
         employeeService.deleteEmployee(employeeId);
@@ -45,7 +42,7 @@ public class EmployeeRestController {
         return employee;
     }
 
-    @PutMapping("/api/employees/{employeeId}")
+    @PutMapping("/{employeeId}")
     public Employee updateEmployee(@RequestBody Employee employee,
                                @PathVariable(name="employeeId")Long employeeId){
         Employee emp = employeeService.getEmployee(employeeId);
