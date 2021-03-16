7412f0080fa85857a591cd8ef6898870ed669d21
David <dvaquero@bilbomatica.es>
2021-03-16 19:02:08 +0100

    mejora del ejemplo para incluir las validaciones en las nuevas versiones de spring data jpa

--- src/main/java/com/cursosdedesarrollo/springbootresth2/controllers/EmployeeRestController.java
+++ src/main/java/com/cursosdedesarrollo/springbootresth2/controllers/EmployeeRestController.java
@@ -10,6 +10,17 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+/*
+    URLS de acceso
+    Clase: Define la url base para un recurso
+    Peticion : JSON
+    Respuesta: code 200 -> JSON
+    List: Get / -> List<Item>
+    Create: Post / (item) -> Item
+    Read: Get /{id} (id) -> Item
+    Update: Post/Put/Patch /{id} (id, item) -> Item
+    Delete: Delete /{id} (id) -> Item
+ */
 
 @RestController
 @RequestMapping("/api/employees")
@@ -18,9 +29,11 @@ public class EmployeeRestController {
     @Autowired
     private EmployeeService employeeService;
 
-
+    //List
     @GetMapping
     public List<Employee> getEmployees() {
+        // al definir el valor que devuelve la función
+        // es toy diciendo que datos deben ser serializados
         List<Employee> employees
                 = employeeService.retrieveEmployees();
         /*
@@ -32,7 +45,7 @@ public class EmployeeRestController {
          */
         return employees;
     }
-
+    // Create
     @PostMapping
     public Employee saveEmployee(
             @Valid @RequestBody Employee employee){
@@ -41,7 +54,7 @@ public class EmployeeRestController {
         System.out.println(employee);
         return employee;
     }
-
+    //Show/Read
     @GetMapping("/{id}")
     public Employee getEmployee(@PathVariable(name="id")Long id) {
         Employee employee=employeeService.getEmployee(id);
@@ -54,19 +67,20 @@ public class EmployeeRestController {
     }
 
 
-
-    @DeleteMapping("/{employeeId}")
+    // Delete
+    @DeleteMapping("/{id}")
     public Employee deleteEmployee(
-            @PathVariable(name="employeeId")Long employeeId){
+            @PathVariable(name="id")Long employeeId){
         Employee employee= employeeService.getEmployee(employeeId);
         employeeService.deleteEmployee(employeeId);
         System.out.println("Employee Deleted Successfully");
         return employee;
     }
 
-    @PutMapping("/{employeeId}")
+    // Update
+    @PutMapping("/{id}")
     public Employee updateEmployee(@Valid @RequestBody Employee employee,
-                               @PathVariable(name="employeeId")Long employeeId){
+                               @PathVariable(name="id")Long employeeId){
         Employee emp = employeeService.getEmployee(employeeId);
         //emp.setName(employee.getName());
         if(emp != null){
